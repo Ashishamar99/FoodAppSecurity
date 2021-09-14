3c7f7a4eec4776a4f38b43aa60a893b41c58f722
Ashish Amar <ashishamar1999@gmail.com>
2021-09-14 20:02:54 +0530

    Created a skeleton spring boot project for security

---
+++ src/test/java/com/brillio/foodapp/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brillio.foodapp.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
